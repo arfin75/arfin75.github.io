e3d761e77a5b79fce9203e3a8e9619b5ab4cf5f8
arfin75 <sf.arfin75@gmail.com>
2020-04-11 22:46:12 -0400

    lading page update

--- jsRestaurants/script.js/restaurant.js
+++ jsRestaurants/script.js/restaurant.js
@@ -1,22 +1,28 @@
 function validateForm() {
 
   let name = document.getElementById("name1").value;
-  // let email = document.getElementById("email").value;
-  // let phone = Number(document.getElementById("phone").value);
-  // console.log(name);
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  console.log(name);
+  console.log(email);
+  console.log(number);
+
   if (name == "") {
-    alert ("Please enter the full name")
     document.getElementById("error1").style.display = "block";
     return false;
-    //  alert("Please enter your name");
+  alert ("Please enter your full name");
+}
+
+  if (email == "") {
+  document.getElementById("error1").style.display = "block";
+  return false;
+   alert ("Please enter your email");
+}
+
+   if (phone == "") {
+  document.getElementById("error1").style.display = "block";
+  return false;
+  alert ("Please enter your phone number");
+
   }
-  // if (email == "") {
-  //   document.getElementById("error1").style.display = "block";
-  //   return false;
-  // }
-  // if (phone == "") {
-  //   document.getElementById("error1").style.display = "block";
-  //   return false;
-  // }
-return false;
 }
